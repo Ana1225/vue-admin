97e6e00256781633c72997f1da39b91c5b7ed91f
Ana1225 <2829330954@qq.com>
2020-08-12 14:02:38 +0800

    构建项目，配置全局，路由，element ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="login">登录页</div>
+</template>
+<script>
+export default {
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background: #5cbec1;
+}
+</style>
